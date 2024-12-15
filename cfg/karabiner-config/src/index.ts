738a8c57df00a38400d663eb93207625bf06b4ff
Sherwin Yu <sherwinxyu@gmail.com>
2024-12-15 10:55:20 -0800

    Update karabiner.ts

--- cfg/karabiner-config/src/index.ts
+++ cfg/karabiner-config/src/index.ts
@@ -1,32 +1,112 @@
 import {
-  layer,
+  FromAndToKeyCode,
+  FromKeyParam,
   map,
-  NumberKeyValue,
+  mapSimultaneous,
   rule,
+  simlayer,
+  toKey,
+  ToKeyParam,
+  toNotificationMessage,
+  toRemoveNotificationMessage,
   withMapper,
-  writeToProfile,
+  writeToProfile
 } from 'karabiner.ts'
+
+// TO RUN: `npm run build`
 
 // ! Change '--dry-run' to your Karabiner-Elements Profile name.
 // (--dry-run print the config json into console)
 // + Create a new profile if needed.
-writeToProfile('--dry-run', [
-  // It is not required, but recommended to put symbol alias to layers,
-  // (If you type fast, use simlayer instead, see https://evan-liu.github.io/karabiner.ts/rules/simlayer)
-  // to make it easier to write '←' instead of 'left_arrow'.
-  // Supported alias: https://github.com/evan-liu/karabiner.ts/blob/main/src/utils/key-alias.ts
-  layer('/', 'symbol-mode').manipulators([
-    //     / + [ 1    2    3    4    5 ] =>
-    withMapper(['⌘', '⌥', '⌃', '⇧', '⇪'])((k, i) =>
-      map((i + 1) as NumberKeyValue).toPaste(k),
-    ),
-    withMapper(['←', '→', '↑', '↓', '␣', '⏎', '⇥', '⎋', '⌫', '⌦', '⇪'])((k) =>
-      map(k).toPaste(k),
-    ),
-  ]),
+writeToProfile('karabiner-config-profile', [
+  // rule('z to ctrl').manipulators([
+  //   map('z', 'optionalAny')
+  //     .toIfAlone('z', {}, { halt: true })
+  //     .toIfHeldDown('left_control')
+  //     .toDelayedAction([], toKey('z'))
+  //     .parameters({
+  //       "basic.to_delayed_action_delay_milliseconds": 500,
+  //       "basic.to_if_held_down_threshold_milliseconds": 200
+  //     })
+  // ]),
 
-  rule('Key mapping').manipulators([
-    // config key mappings
-    map(1).to(1)
+
+  // rule("A")
+  //   .manipulators([
+  //     withModifier('optionalAny')([
+  //       map('a')
+  //         .toIfAlone("a", {}, { halt: true })
+  //         .toIfHeldDown("‹⇧"),
+  //     ]),
+  //   ]),
+  // rule("easy mods").manipulators([
+  //   withModifier('optionalAny')([
+  //     map('z')
+  //       .toIfAlone("z", {}, { halt: true })
+  //       .toIfHeldDown("left_control"),
+  //     map('/')
+  //       .toIfAlone("/", {}, { halt: true })
+  //       .toIfHeldDown("left_control"),
+  //   ]),
+  // ]),
+
+  text_editing_layer(),
+  nav_layer(),
+  easy_mods_rules(),
+  // symbol_layer(),
+  rule('jk => Esc').manipulators([
+    mapSimultaneous(['j', 'k']).to('escape'),
+  ]),
+  rule('Caps Lock → Hyper').manipulators([
+    map('caps_lock').toHyper().toIfAlone('caps_lock'),
   ]),
 ])
+
+function text_editing_layer() {
+  return simlayer('e', 'text_editing').manipulators([
+    map('a').to('a', 'command'),
+    map('s').to('s', ['control', 'command']),
+    map('p').to('left_arrow', 'command').to('right_arrow', ['shift', 'command']),
+    map('k').to('up_arrow', 'control'),
+    map('j').to('down_arrow', 'control')
+  ])
+}
+
+function nav_layer() {
+  return simlayer('d', 'nav')
+    .toIfActivated(toNotificationMessage('simlayer-d', 'Navigation Mode'))
+    .toIfDeactivated(toRemoveNotificationMessage('simlayer-d'))
+    .manipulators([
+      map('h').to('left_arrow'),
+      map('l').to('right_arrow'),
+      map('k').to('up_arrow'),
+      map('j').to('down_arrow'),
+
+      map('a').to('left_arrow', 'command'),
+      map(';').to('right_arrow', 'command'),
+
+      map('f').to('left_option'),
+      map('s').to('left_shift'),
+    ])
+}
+
+
+function easy_mods_rules() {
+  return rule('easy mods').manipulators([
+    withMapper<ToKeyParam, ToKeyParam>({
+      'z': 'left_control',
+      'slash': 'right_control',
+      'f': 'left_option',
+    })((lhs, rhs) => {
+      return map(lhs as FromKeyParam, 'optionalAny')
+        .toIfAlone(lhs as ToKeyParam, {}, { halt: true })
+        .toIfHeldDown(rhs)
+        .toDelayedAction([], toKey(lhs))
+        .parameters({
+          "basic.to_delayed_action_delay_milliseconds": 500,
+          "basic.to_if_held_down_threshold_milliseconds": 160
+        })
+    })
+  ])
+}
+
